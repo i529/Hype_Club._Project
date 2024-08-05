374fe0775a7ad11d52d9da06a0c4cb755689b4c6
i529 <icaromf@hotmail.com>
2024-08-05 11:58:19 -0300

    Primeiro commit do projeto hype club.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { TrabalhosComponent } from './trabalhos/trabalhos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreNosComponent,
+    TrabalhosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
